c606d67cec10e57a682f66660fbde66f55d66d9c
Abhishek Kumar <skylake1302@gmail.com>
2021-09-16 18:17:16 +0200

    Enabled CORS

--- backend/src/api/app.ts
+++ backend/src/api/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 
 import { routes } from './routes';
 import { errorLogger } from './middlewares/error-handlers';
@@ -7,6 +8,7 @@ import { errorLogger } from './middlewares/error-handlers';
 const app = express();
 
 // Pre Middlewares
+app.use(cors())
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
