a53d931e4553f392358898a6e6bc729d1c68504e
Abhishek Kumar <skylake1302@gmail.com>
2021-09-21 12:25:21 +0200

    infinite scroll: initial working commit

--- frontend/src/app/aq/components/aq-list/aq-list.component.ts
+++ frontend/src/app/aq/components/aq-list/aq-list.component.ts
@@ -1,25 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 import { AirQualityModel } from '../../interfaces';
 import { AqListService } from '../../services/aq-list/aq-list.service';
 
-const limit = 20;
+const limit = 50;
 @Component({
   selector: 'app-aq-list',
   templateUrl: './aq-list.component.html',
   styleUrls: ['./aq-list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
   public aqData: AirQualityModel[] = [];
+  private intersection: IntersectionObserver;
+  private isIntersecting = new Subject<boolean>();
 
-  constructor(private readonly aqListService: AqListService) { }
+  @ViewChild('bottom')
+  public bottom!: ElementRef;
+
+  constructor(private readonly aqListService: AqListService) {
+    this.intersection = new IntersectionObserver((entries) => {
+          const firstEntry = entries[0];
+          this.isIntersecting.next(firstEntry.isIntersecting);
+      },
+      { threshold: 0 });
+   }
 
   public ngOnInit(): void {
-    this.aqListService.getAirQualityData(this.page, limit)
-      .subscribe((aqData) => {
-        this.aqData = aqData;
-      });
+    this.isIntersecting.subscribe((is) => {
+      console.log('intersecting', is);
+      if(is) {
+        this.page = this.page + 1;
+        this.aqListService.getAirQualityData(this.page, limit)
+          .subscribe((aqData) => {
+            this.aqData = [...this.aqData, ...aqData];
+          });
+      }
+      
+    });
   }
 
-  private page = 2;
+  public ngAfterViewInit() {
+    this.intersection.observe(this.bottom.nativeElement);
+  }
+
+  public ngOnDestroy() {
+    this.intersection.disconnect();
+  }
+
+  public trackBy(i: number) {
+    return i;
+  }
+
+  private page = 0;
 }
